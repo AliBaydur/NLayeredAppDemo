3f4ac6fdbd0189bf1c2d367bf271e41d9c6fb845
AliBaydur <59739580+AliBaydur@github.com>
2021-10-17 02:07:55 +0300

    Add project files.

---
+++ Northwind.Business/Concreate/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Northwind.Business.Abstract;
+using Northwind.DataAccess.Abstract;
+using Northwind.DataAccess.Concreate;
+using Northwind.DataAccess.Concreate.EntityFramework;
+using Northwind.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concreate
+{
+    public class ProductManager:IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Business code
+            return _productDal.GetAll();
+        }
+    }
+}
